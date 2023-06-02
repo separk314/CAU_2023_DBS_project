7cebf73c5ffc5392246eaa7df018691b3c9bcf06
soeun <separk314@naver.com>
2023-06-02 16:57:01 +0900

    Feat: Bitmap 구현

--- DBS_project_lab/src/Bitmap.java
+++ DBS_project_lab/src/Bitmap.java
@@ -4,27 +4,59 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bitmap {
-    private Map<String, BitSet> index;
-    String bitmapName;
+    private Map<String, BitSet> index = new HashMap<>();
+    private String bitmapName;
 
     public Bitmap(String bitmapName) {
         this.bitmapName = bitmapName;
-        this.index = new HashMap<>();
     }
 
-    public void addKey(String key, int serialId) {
+    public void add(String key, int serialId) {
         BitSet bitSet = index.getOrDefault(key, new BitSet());
         bitSet.set(serialId);
         index.put(key, bitSet);
     }
 
-    public ArrayList<Integer> processMultipleKeyQuery() {
-        ArrayList<Integer> result = new ArrayList<>();
-        return result;
+    public Bitmap processMultipleKeyQuery(Map<Bitmap, String> queries) {
+        BitSet result = null;
+
+        for(Map.Entry<Bitmap, String> query: queries.entrySet()) {
+            Bitmap queryBitmap = query.getKey();    // ex. "Korea"
+            BitSet bitmapIndex = queryBitmap.getIndex().get(query.getValue());  // Korea의 index를 가져온다
+
+            if (bitmapIndex == null) {
+                // 만들어둔 index가 없는 경우
+                result = null;
+                break;
+            } else {
+                if (result == null) {
+                    // for문의 첫 번째 반복인 경우
+                    result = bitmapIndex;
+                } else {
+                    BitSet bitset = (BitSet) result.clone();
+                    bitset.and(bitmapIndex);
+                    result = bitset;
+                }
+            }
+        }
+
+        if (result != null) {
+            Bitmap resultBitmap = new Bitmap("Multiple key");
+            resultBitmap.getIndex().put("queryResult", result);
+            return resultBitmap;
+        }
+
+        return new Bitmap("Multiple key");
     }
 
-    public ArrayList<Integer> processCountQuery() {
-        ArrayList<Integer> result = new ArrayList<>();
-        return result;
+    public int processCountQuery(String key) {
+        if(index.get(key) == null) {
+            return 0;
+        } else {
+            return index.get(key).cardinality();
+        }
     }
+
+    public String getBitmapName() { return bitmapName;  }
+    public Map<String, BitSet> getIndex()   {   return index;   }
 }
--- DBS_project_lab/src/Controller.java
+++ DBS_project_lab/src/Controller.java
@@ -1,5 +1,138 @@
-public class Controller {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Controller {
+    static int serialId = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Bitmap genderIndex = new Bitmap("genderIndex");
+        Bitmap countryIndex = new Bitmap("countryIndex");
+        Bitmap gradeIndex = new Bitmap("gradeIndex");
+
+        while (true) {
+            System.out.println("< 고객 관리 Application >");
+            System.out.println("1. record 삽입");
+            System.out.println("2. DB에 있는 bitmap index 확인");
+            System.out.println("3. multiple-key 질의");
+            System.out.println("4. 집계함수 count(*) 처리");
+            System.out.println("5. 종료");
+            System.out.print("숫자를 입력해주세요: ");
+
+            int result = scanner.nextInt();
+
+            if (result == 1) {
+                System.out.println("< 생성할 고객 정보를 입력해주세요 >");
+                System.out.print("고객 이름을 입력하세요: ");
+                String name = scanner.next();
+
+                int gender = 0;
+                while (true) {
+                    System.out.println("성별을 선택하세요: 남자는 1, 여자는 2를 입력");
+                    int genderInt = scanner.nextInt();
+                    if (genderInt == 1) {
+                        gender = 'm';
+                        break;
+                    } else if (genderInt == 2) {
+                        gender = 'f';
+                        break;
+                    } else {
+                        System.out.println("다시 입력해주세요.");
+                    }
+                }
+
+                System.out.print("고객이 거주 중인 나라를 입력하세요: ");
+                String country = scanner.next();
+
+                int grade = 0;
+                while (true) {
+                    System.out.println("고객 등급을 입력하세요: 1~4 중에서 입력");
+                    int gradeInt = scanner.nextInt();
+                    if (gradeInt >= 1 && gradeInt <= 4) {
+                        grade = gradeInt;
+                        break;
+                    } else {
+                        System.out.println("다시 입력해주세요.");
+                    }
+                }
+
+            } else if (result == 2) {
+                System.out.println("gender, country, grade index가 있습니다.");
+
+            } else if (result == 3) {
+
+            } else if (result == 4) {
+                while (true) {
+                    System.out.println("집계 함수를 실행할 column을 선택하세요.");
+                    System.out.println("1. gender(성별)");
+                    System.out.println("2. country(나라)");
+                    System.out.println("3. grade(고객 등급)");
+                    result = scanner.nextInt();
+
+                    if (result == 1) {
+                        System.out.println("gender key값을 입력하세요: 남자는 1, 여자는 2를 선택");
+                        int genderScan = scanner.nextInt();
+                        if (genderScan == 1) {
+                            System.out.println(genderIndex.processCountQuery("m"));
+                        } else if (genderScan == 2) {
+                            System.out.println(genderIndex.processCountQuery("f"));
+                        } else {
+                            System.out.println("< Invalid input >");
+                        }
+                        break;
+
+                    } else if (result == 2) {
+                        System.out.print("country key값을 입력하세요: ");
+                        String countryScan = scanner.next();
+                        System.out.println(genderIndex.processCountQuery(countryScan));
+                        break;
+
+                    } else if (result == 3) {
+                        System.out.println("grade key값을 입력하세요: 1~4 중에서 선택");
+                        int gradeScan = scanner.nextInt();
+                        if (gradeScan >= 1 && gradeScan <= 4) {
+                            System.out.println(genderIndex.processCountQuery(String.valueOf(gradeScan)));
+                        } else {
+                            System.out.println("< Invalid input >");
+                        }
+                        break;
+
+                    } else {
+                        System.out.println("다시 입력해주세요.");
+                    }
+                }
+            } else if (result == 5) {
+                System.out.println("< 종료 >");
+                break;
+            } else {
+                System.out.println("\n<Invalid Input> 다시 입력해주세요");
+            }
+        }
     }
 }
+//
+//
+//        genderIndex.add("m", 1);
+//        genderIndex.add("m", 2);
+//        genderIndex.add("f", 3);
+//        genderIndex.add("f", 4);
+//        genderIndex.add("f", 5);
+//
+//        countryIndex.add("Korea", 1);
+//        countryIndex.add("Japan", 2);
+//        countryIndex.add("Korea", 3);
+//        countryIndex.add("Japan", 4);
+//        countryIndex.add("Japan", 5);
+//
+//        Map<Bitmap, String> queryMap = new HashMap<>();
+//        queryMap.put(genderIndex, "f");
+//        queryMap.put(countryIndex, "Japan");
+//
+//        Bitmap result = genderIndex.processMultipleKeyQuery(queryMap);
+//        if (result != null) {
+//            System.out.println("Result: " + result.getIndex().get("queryResult"));
+//        } else {
+//            System.out.println("No result found.");
+//        }
+//    }
+//}
+//
